b38bf25ba54c7ca5b26527a4e40bfe3bd145489b
Igor Lezhnin <i.lezhnin@redmadrobot.com>
2025-06-24 01:19:09 +0700

    Initial commit with k8s

---
+++ frontend/src/config/serverApiConfig.js
@@ -0,0 +1,16 @@
+export const API_BASE_URL =
+    import.meta.env.VITE_BACKEND_SERVER + 'api/';
+export const BASE_URL =
+  import.meta.env.VITE_BACKEND_SERVER
+
+
+export const DOWNLOAD_BASE_URL =
+  import.meta.env.VITE_BACKEND_SERVER + 'download/'
+
+export const FILE_BASE_URL = import.meta.env.VITE_FILE_BASE_URL;
+
+console.log(API_BASE_URL);
+
+//  console.log(
+//    '🚀 Welcome to IDURAR ERP CRM! Did you know that we also offer commercial customization services? Contact us at hello@idurarapp.com for more information.'
+//  );
